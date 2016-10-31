eeb44794bcbe900c9e03e785c8c38f18fafa0141
Sara El Nahal <SaraNahal@Saras-MacBook-Pro.local>
2016-10-31 18:27:38 +0200

    fix database migarte error

--- config/database.php
+++ config/database.php
@@ -55,7 +55,6 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            // 'host' => env('DB_HOST', '127.0.0.1') . ('homestead' == gethostname() ? null: ':33060'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'codetouch'),
             'username' => env('DB_USERNAME', 'homestead'),
